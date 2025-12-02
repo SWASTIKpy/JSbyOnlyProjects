4a80bd387fea9e10a633e558f0311b3ae7329aa3
swastikPY <swastikvishwa908@gmail.com>
2025-12-02 16:26:32 +0530

    made some changes in js and is completly functional now

--- QuoteGen/script.js
+++ QuoteGen/script.js
@@ -2,7 +2,16 @@ const qCon = document.querySelector("#quoteDisplay")
 const btn = document.querySelector("#genQ")
 
 btn.addEventListener('click', () => {
-    fetch("https://cors-anywhere.herokuapp.com/https://zenquotes.io/api/random").then(res => {
-        console.log(res)
-    })
+    // can use promise then chaining or use await dont use both it can be bad will use promise as woh samjh nhi aaya abhi
+    fetch("https://dummyjson.com/quotes/random") // fetch quotes
+    // .json convert response object to apna use wala object for js unpacking basically
+    .then(res => res.json()) // convert it to json 
+    .then(data => {
+        const p = document.createElement('p')
+        p.classList.add("quote-para")
+        p.textContent = `${data.quote} - ${data.author}`
+        qCon.appendChild(p)
+})
+.catch(err => console.error("tu quote dekhta hoga laadle ham toh error dikhate",err))
+
 })
